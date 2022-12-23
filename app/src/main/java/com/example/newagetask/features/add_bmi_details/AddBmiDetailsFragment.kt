38dd81ecf2c11c546b99c61e43b573a74b08378c
macbookpro <aslm.monir@paysky.io>
2022-12-23 22:54:15 +0200

    added adapter for Weight / height / Gender

--- app/src/main/java/com/example/newagetask/features/add_bmi_details/AddBmiDetailsFragment.kt
+++ app/src/main/java/com/example/newagetask/features/add_bmi_details/AddBmiDetailsFragment.kt
@@ -1,25 +1,22 @@
 package com.example.newagetask.features.add_bmi_details
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newagetask.R
-import com.example.newagetask.features.add_bmi_details.adapter.BmiWeightCreatorAdapter
-import com.example.newagetask.features.add_bmi_details.adapter.orderItem
+import com.example.newagetask.features.add_bmi_details.adapter.BmiCreatorAdapter
+import com.example.newagetask.features.add_bmi_details.adapter.PersonData
 import com.google.android.material.button.MaterialButton
 
 
 class AddBmiDetailsFragment : Fragment() {
-    lateinit var bmiWeightCreatorAdapter: BmiWeightCreatorAdapter
-    lateinit var bmiHeightCreatorAdapter: BmiWeightCreatorAdapter
-    lateinit var bmiGenderCreatorAdapter: BmiWeightCreatorAdapter
+    lateinit var bmiWeightCreatorAdapter: BmiCreatorAdapter
+    lateinit var bmiHeightCreatorAdapter: BmiCreatorAdapter
+    lateinit var bmiGenderCreatorAdapter: BmiCreatorAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +24,9 @@ class AddBmiDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_add_bmi_details, container, false)
-        bmiWeightCreatorAdapter = BmiWeightCreatorAdapter()
-        bmiHeightCreatorAdapter = BmiWeightCreatorAdapter()
-        bmiGenderCreatorAdapter = BmiWeightCreatorAdapter()
+        bmiWeightCreatorAdapter = BmiCreatorAdapter()
+        bmiHeightCreatorAdapter = BmiCreatorAdapter()
+        bmiGenderCreatorAdapter = BmiCreatorAdapter()
 
         return view.rootView
     }
@@ -37,23 +34,43 @@ class AddBmiDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bmiWeightCreatorAdapter.differ.submitList(mutableListOf(orderItem(1),
-            orderItem(2),orderItem(3),orderItem(4),orderItem(5),orderItem(6),orderItem(7),orderItem(8),orderItem(9)
-        ))
-
-        bmiHeightCreatorAdapter.differ.submitList(mutableListOf(orderItem(1),
-            orderItem(2),orderItem(3),orderItem(4),orderItem(5),orderItem(6),orderItem(7),orderItem(8),orderItem(9)
-        ))
-        bmiGenderCreatorAdapter.differ.submitList(mutableListOf(orderItem(1),
-            orderItem(2),orderItem(3),orderItem(4),orderItem(5),orderItem(6),orderItem(7),orderItem(8),orderItem(9)
-        ))
-
-        view.findViewById<RecyclerView>(R.id.rv_height).adapter = bmiHeightCreatorAdapter
-        view.findViewById<RecyclerView>(R.id.rv_gender).adapter = bmiGenderCreatorAdapter
+        initBmiCreatorData(view)
 
         view.findViewById<MaterialButton>(R.id.btn_calculate).setOnClickListener {
             findNavController().navigate(R.id.goToBmiDetailsFragment)
         }
+    }
+
+    private fun initBmiCreatorData(view: View) {
+        bmiWeightCreatorAdapter.differ.submitList(
+            mutableListOf(
+                PersonData("1"),
+                PersonData("2"),
+                PersonData("3"),
+                PersonData("4"),
+                PersonData("5"),
+
+            )
+        )
+
+        bmiHeightCreatorAdapter.differ.submitList(
+            mutableListOf(
+                PersonData("1"),
+                PersonData("2"),
+                PersonData("3"),
+                PersonData("4"),
+                PersonData("5"),
+            )
+        )
+        bmiGenderCreatorAdapter.differ.submitList(
+            mutableListOf(
+                PersonData("Male"),
+                PersonData("Female"),
+            )
+        )
+        view.findViewById<RecyclerView>(R.id.rv_weight).adapter = bmiWeightCreatorAdapter
+        view.findViewById<RecyclerView>(R.id.rv_height).adapter = bmiHeightCreatorAdapter
+        view.findViewById<RecyclerView>(R.id.rv_gender).adapter = bmiGenderCreatorAdapter
     }
 
 
--- app/src/main/java/com/example/newagetask/features/add_bmi_details/adapter/BmiCreatorAdapter.kt
+++ app/src/main/java/com/example/newagetask/features/add_bmi_details/adapter/BmiCreatorAdapter.kt
@@ -1,5 +1,4 @@
 package com.example.newagetask.features.add_bmi_details.adapter
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,116 +9,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.newagetask.R
 
 
-const val header_layout_item=0
-const val item_layout_item=1
-
-//class BmiCreatorAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-//
-//
-//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-//        when (holder) {
-//            is ViewHolderheaderLayout -> {
-//                holder.setData()
-//                holder.setIsRecyclable(false)
-//            }
-//            is ViewHolderItemLayout -> {
-//                holder.setData(differ.currentList[position-1])
-//                holder.setIsRecyclable(false)
-//            }
-//        }
-//
-//    }
-//
-//    override fun onCreateViewHolder(
-//        parent: ViewGroup,
-//        viewType: Int
-//    ): RecyclerView.ViewHolder {
-//        when (viewType) {
-//            header_layout_item -> {
-//                val view: View = LayoutInflater.from(parent.context)
-//                    .inflate(R.layout.bmi_item_header_layout, parent, false)
-//                return ViewHolderheaderLayout(view)
-//            }
-//            item_layout_item -> {
-//                val view: View = LayoutInflater.from(parent.context)
-//                    .inflate(R.layout.bmi_item_layout, parent, false)
-//                return ViewHolderItemLayout(view)
-//            }
-//           else -> {
-//               val view: View = LayoutInflater.from(parent.context)
-//                   .inflate(R.layout.bmi_item_layout, parent, false)
-//               return ViewHolderItemLayout(view)
-//           }
-//       }
-//    }
-//
-//    override fun getItemCount() = differ.currentList.size +1
-//
-//    inner class ViewHolderItemLayout(var view: View) : RecyclerView.ViewHolder(view) {
-//        fun setData(item: orderItem) {
-//            view.findViewById<TextView>(R.id.tv_weight).text = item.id.toString()
-////            view.findViewById<TextView>(R.id.tv_height).text = item.id.toString()
-////            view.findViewById<TextView>(R.id.tv_gender).text = item.id.toString()
-//
-//        }
-//
-//    }
-//
-//    inner class ViewHolderheaderLayout(var view: View) : RecyclerView.ViewHolder(view) {
-//        fun setData() {
-//            view.findViewById<TextView>(R.id.tv_weight_title).text = view.resources.getString(R.string.weightTitle)
-//            view.findViewById<TextView>(R.id.tv_height_title).text = view.resources.getString(R.string.heightTitle)
-//            view.findViewById<TextView>(R.id.tv_gender_title).text = view.resources.getString(R.string.genderTitle)
-//
-//        }
-//
-//    }
-//
-//    override fun getItemViewType(position: Int): Int {
-//        if (isPositionHeader(position))
-//            return header_layout_item
-//        return item_layout_item
-//    }
-//
-//    private fun isPositionHeader(position: Int): Boolean {
-//        return position == 0
-//    }
-//    private val differCallback = object : DiffUtil.ItemCallback<orderItem>() {
-//        override fun areItemsTheSame(oldItem: orderItem, newItem: orderItem): Boolean {
-//            return oldItem.id == newItem.id
-//        }
-//
-//        override fun areContentsTheSame(oldItem: orderItem, newItem: orderItem): Boolean {
-//            return oldItem == newItem
-//        }
-//
-//    }
-//    val differ = AsyncListDiffer(this, differCallback)
-//
-//}
-data class orderItem(var id:Int)
-
-
-
-class BmiWeightCreatorAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private var selectedItem = -1
-
+class BmiCreatorAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         (holder as ViewHolderItemLayout).setData(differ.currentList[position])
         holder.setIsRecyclable(false)
-        if (position == selectedItem) {
-//            (holder as ViewHolderItemLayout).view.findViewById<TextView>(R.id.tv_weight).setTextColor(
-//                Color.parseColor("#094673"));
-        } else {
-//            (holder as ViewHolderItemLayout).view.findViewById<TextView>(R.id.tv_weight).setTextColor(
-//                Color.GRAY)
-        }
     }
-    fun setSelecteditem(selecteditem: Int) {
-        selectedItem = selecteditem
-        //notifyDataSetChanged()
-    }
-
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -132,21 +26,16 @@ class BmiWeightCreatorAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() 
     override fun getItemCount() = differ.currentList.size
 
     inner class ViewHolderItemLayout(var view: View) : RecyclerView.ViewHolder(view) {
-        fun setData(item: orderItem) {
-            view.findViewById<TextView>(R.id.tv_weight).text = item.id.toString()
-//            view.findViewById<TextView>(R.id.tv_height).text = item.id.toString()
-//            view.findViewById<TextView>(R.id.tv_gender).text = item.id.toString()
-
+        fun setData(item: PersonData) {
+            view.findViewById<TextView>(R.id.tv_item_text).text = item.id.toString()
         }
-
     }
-
-    private val differCallback = object : DiffUtil.ItemCallback<orderItem>() {
-        override fun areItemsTheSame(oldItem: orderItem, newItem: orderItem): Boolean {
+    private val differCallback = object : DiffUtil.ItemCallback<PersonData>() {
+        override fun areItemsTheSame(oldItem: PersonData, newItem: PersonData): Boolean {
             return oldItem.id == newItem.id
         }
 
-        override fun areContentsTheSame(oldItem: orderItem, newItem: orderItem): Boolean {
+        override fun areContentsTheSame(oldItem: PersonData, newItem: PersonData): Boolean {
             return oldItem == newItem
         }
 
@@ -155,3 +44,5 @@ class BmiWeightCreatorAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() 
 
 
 }
+data class PersonData(var id:String)
+
