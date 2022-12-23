555e4bdc292c4beec530597d1c24878c045b9973
macbookpro <aslm.monir@paysky.io>
2022-12-23 20:27:55 +0200

    added Splash Screen Design

--- app/src/main/java/com/example/newagetask/MainActivity.kt
+++ app/src/main/java/com/example/newagetask/MainActivity.kt
@@ -2,19 +2,16 @@ package com.example.newagetask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
-import androidx.core.content.ContextCompat
-import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setAppTheme()
         setContentView(R.layout.activity_main)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
 
     }
 
-    fun getNavController() = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    private fun setAppTheme() = setTheme(R.style.Theme_NewAgeTask)
+
 }
