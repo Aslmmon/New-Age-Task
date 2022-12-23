2bc7d374161ed8b58ca93b5282928106fc1cbe7e
macbookpro <aslm.monir@paysky.io>
2022-12-23 22:49:07 +0200

    added recyclerAdapter for each item

---
+++ app/src/main/java/com/example/newagetask/common/CustomBmiCreator.kt
@@ -0,0 +1,24 @@
+package com.example.newagetask.common
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.newagetask.R
+
+class CustomBmiCreator @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : ConstraintLayout(context, attrs, defStyle) {
+    private val TAG = CustomToolbar::class.simpleName
+
+
+    init {
+        (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(
+            R.layout.custom_bmi_creator,
+            this,
+            true
+        )
+    }
+}
--- app/src/main/java/com/example/newagetask/features/add_bmi_details/AddBmiDetailsFragment.kt
+++ app/src/main/java/com/example/newagetask/features/add_bmi_details/AddBmiDetailsFragment.kt
@@ -1,37 +1,60 @@
 package com.example.newagetask.features.add_bmi_details
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.navigation.findNavController
+import android.view.ViewTreeObserver
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.newagetask.MainActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newagetask.R
+import com.example.newagetask.features.add_bmi_details.adapter.BmiWeightCreatorAdapter
+import com.example.newagetask.features.add_bmi_details.adapter.orderItem
 import com.google.android.material.button.MaterialButton
 
 
 class AddBmiDetailsFragment : Fragment() {
-
-
+    lateinit var bmiWeightCreatorAdapter: BmiWeightCreatorAdapter
+    lateinit var bmiHeightCreatorAdapter: BmiWeightCreatorAdapter
+    lateinit var bmiGenderCreatorAdapter: BmiWeightCreatorAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_bmi_details, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_bmi_details, container, false)
+        bmiWeightCreatorAdapter = BmiWeightCreatorAdapter()
+        bmiHeightCreatorAdapter = BmiWeightCreatorAdapter()
+        bmiGenderCreatorAdapter = BmiWeightCreatorAdapter()
+
+        return view.rootView
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        bmiWeightCreatorAdapter.differ.submitList(mutableListOf(orderItem(1),
+            orderItem(2),orderItem(3),orderItem(4),orderItem(5),orderItem(6),orderItem(7),orderItem(8),orderItem(9)
+        ))
+
+        bmiHeightCreatorAdapter.differ.submitList(mutableListOf(orderItem(1),
+            orderItem(2),orderItem(3),orderItem(4),orderItem(5),orderItem(6),orderItem(7),orderItem(8),orderItem(9)
+        ))
+        bmiGenderCreatorAdapter.differ.submitList(mutableListOf(orderItem(1),
+            orderItem(2),orderItem(3),orderItem(4),orderItem(5),orderItem(6),orderItem(7),orderItem(8),orderItem(9)
+        ))
+
+        view.findViewById<RecyclerView>(R.id.rv_height).adapter = bmiHeightCreatorAdapter
+        view.findViewById<RecyclerView>(R.id.rv_gender).adapter = bmiGenderCreatorAdapter
 
         view.findViewById<MaterialButton>(R.id.btn_calculate).setOnClickListener {
-            Toast.makeText(requireContext(), "Hello", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.goToBmiDetailsFragment)
         }
     }
+
+
 }
