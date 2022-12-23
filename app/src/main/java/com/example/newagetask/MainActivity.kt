b0ecd68449a3cf18f6e6e066745b773b9c29026e
macbookpro <aslm.monir@paysky.io>
2022-12-23 11:33:45 +0200

    added navigation between fragments

--- app/src/main/java/com/example/newagetask/MainActivity.kt
+++ app/src/main/java/com/example/newagetask/MainActivity.kt
@@ -5,10 +5,16 @@ import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
     }
+
+    fun getNavController() = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
 }
--- app/src/main/java/com/example/newagetask/features/add_bmi_details/AddBmiDetailsFragment.kt
+++ app/src/main/java/com/example/newagetask/features/add_bmi_details/AddBmiDetailsFragment.kt
@@ -5,16 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.newagetask.MainActivity
 import com.example.newagetask.R
+import com.google.android.material.button.MaterialButton
 
 
 class AddBmiDetailsFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,4 +25,13 @@ class AddBmiDetailsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add_bmi_details, container, false)
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<MaterialButton>(R.id.btn_calculate).setOnClickListener {
+            Toast.makeText(requireContext(), "Hello", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.goToBmiDetailsFragment)
+        }
+    }
 }
