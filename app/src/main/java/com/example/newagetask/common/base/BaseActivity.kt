2c5f0e9a79b1acf5b5c99024b7b632b28dc2e30a
macbookpro <aslm.monir@paysky.io>
2022-12-25 09:10:25 +0200

    added implementation for Native Banner Ads

--- app/src/main/java/com/example/newagetask/common/base/BaseActivity.kt
+++ app/src/main/java/com/example/newagetask/common/base/BaseActivity.kt
@@ -1,6 +1,5 @@
 package com.example.newagetask.common.base
 
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,76 +7,64 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newagetask.R
 import com.google.android.gms.ads.*
-import com.google.android.gms.ads.formats.MediaView
-import com.google.android.gms.ads.formats.UnifiedNativeAd
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.google.android.gms.ads.nativead.NativeAd
 import com.google.android.gms.ads.nativead.NativeAdView
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
 
 
 const val AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712"
 const val NATIVE_UNIT_ID = "ca-app-pub-3940256099942544/2247696110"
-const val AD_MANAGER_AD_UNIT_ID = "/6499/example/native"
+const val TAG="ad"
 
-
+/**
+ * Needed to be refactored to another separated Class
+ */
 @AndroidEntryPoint
 abstract class BaseActivity : AppCompatActivity() {
-    var currentUnifiedNativeAd: NativeAd? = null
-
+    var nativeAd: NativeAd? = null
     private var mInterstitialAd: InterstitialAd? = null
-    private var adLoader:AdLoader?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setAppTheme()
         setContentView(provideMainLayout())
-
         initializeAds()
 
     }
 
     private fun loadAds() {
-//        val adRequest = AdRequest.Builder().build()
-//        InterstitialAd.load(this, AD_UNIT_ID, adRequest, object : InterstitialAdLoadCallback() {
-//            override fun onAdFailedToLoad(adError: LoadAdError) {
-//                mInterstitialAd = null
-//            }
-//
-//            override fun onAdLoaded(interstitialAd: InterstitialAd) {
-//                mInterstitialAd = interstitialAd
-//            }
-//        })
-//        listenForEventsOfLoadedAd()
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(this, AD_UNIT_ID, adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                mInterstitialAd = interstitialAd
+            }
+        })
+        listenForEventsOfLoadedAd()
     }
 
     fun showNativeAds(nativeAdLayoutId:Int, frame:FrameLayout){
 
        val builder =  AdLoader.Builder(this, NATIVE_UNIT_ID)
         builder.forNativeAd {  nativeAd->
-            // If this callback occurs after the activity is destroyed, you must call
-            // destroy and return or you may get a memory leak.
             var activityDestroyed = false
             activityDestroyed = isDestroyed
             if (activityDestroyed || isFinishing || isChangingConfigurations) {
                  nativeAd.destroy()
                 return@forNativeAd
             }
-
-            // You must call destroy on old ads when you are done with them,
-            // otherwise you will have a memory leak.
-            currentUnifiedNativeAd?.destroy()
-            currentUnifiedNativeAd = nativeAd
-            val adView = layoutInflater.inflate(R.layout.native_add, null,false) as NativeAdView
+            this.nativeAd?.destroy()
+            this.nativeAd = nativeAd
+            val adView = layoutInflater.inflate(nativeAdLayoutId, null,false) as NativeAdView
             populateUnifiedNativeAdView(nativeAd, adView)
             frame.removeAllViews()
             frame.addView(adView)
-
-
-
 
         }
 
@@ -100,24 +87,20 @@ abstract class BaseActivity : AppCompatActivity() {
     private fun listenForEventsOfLoadedAd() {
         mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
             override fun onAdClicked() {
-                // Called when a click is recorded for an ad.
-                Log.e("TAG", "Ad was clicked.")
+                Log.e(TAG, "Ad was clicked.")
             }
 
             override fun onAdDismissedFullScreenContent() {
-                // Called when ad is dismissed.
-                Log.e("TAG", "Ad dismissed fullscreen content.")
+                Log.e(TAG, "Ad dismissed fullscreen content.")
                 mInterstitialAd = null
             }
 
             override fun onAdImpression() {
-                // Called when an impression is recorded for an ad.
-                Log.e("TAG", "Ad recorded an impression.")
+                Log.e(TAG, "Ad recorded an impression.")
             }
 
             override fun onAdShowedFullScreenContent() {
-                // Called when ad is shown.
-                Log.e("TAG", "Ad showed fullscreen content.")
+                Log.e(TAG, "Ad showed fullscreen content.")
             }
         }
     }
@@ -128,26 +111,17 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
     private fun populateUnifiedNativeAdView(nativeAd: NativeAd, adView: NativeAdView) {
-        // Set the media view.
-        //adView.mediaView = adView.findViewById<MediaView>(R.id.ad_media)
-
-        // Set other ad assets.
         adView.headlineView = adView.findViewById(R.id.ad_headline)
         adView.callToActionView = adView.findViewById(R.id.ad_call_to_action)
         adView.iconView = adView.findViewById(R.id.ad_app_icon)
         adView.advertiserView = adView.findViewById(R.id.ad_advertiser)
-
-        // The headline and media content are guaranteed to be in every UnifiedNativeAd.
         (adView.headlineView as TextView).text = nativeAd.headline
-        adView.mediaView?.setMediaContent(nativeAd.mediaContent)
 
-        // These assets aren't guaranteed to be in every UnifiedNativeAd, so it's important to
-        // check before trying to display them.
         if (nativeAd.body == null) {
             adView.bodyView?.visibility = View.INVISIBLE
         } else {
             adView.bodyView?.visibility = View.VISIBLE
-//            (adView.bodyView as TextView).text = nativeAd.body
+            (adView.advertiserView as TextView).text = nativeAd.body
         }
 
         if (nativeAd.callToAction == null) {
@@ -165,37 +139,6 @@ abstract class BaseActivity : AppCompatActivity() {
             )
             adView.iconView?.visibility = View.VISIBLE
         }
-
-        if (nativeAd.price == null) {
-            adView.priceView?.visibility = View.INVISIBLE
-        } else {
-            adView.priceView?.visibility = View.VISIBLE
-           // (adView.priceView as TextView).text = nativeAd.price
-        }
-
-        if (nativeAd.store == null) {
-            adView.storeView?.visibility = View.INVISIBLE
-        } else {
-            adView.storeView?.visibility = View.VISIBLE
-           // (adView.storeView as TextView).text = nativeAd.store
-        }
-
-        if (nativeAd.starRating == null) {
-            adView.starRatingView?.visibility = View.INVISIBLE
-        } else {
-           // (adView.starRatingView as RatingBar).rating = nativeAd.starRating!!.toFloat()
-            adView.starRatingView?.visibility = View.VISIBLE
-        }
-
-        if (nativeAd.advertiser == null) {
-            adView.advertiserView?.visibility = View.INVISIBLE
-        } else {
-            (adView.advertiserView as TextView).text = nativeAd.advertiser
-            adView.advertiserView?.visibility = View.VISIBLE
-        }
-
-        // This method tells the Google Mobile Ads SDK that you have finished populating your
-        // native ad view with this native ad.
         adView.setNativeAd(nativeAd)
 
     }
@@ -205,15 +148,15 @@ abstract class BaseActivity : AppCompatActivity() {
         if (mInterstitialAd != null) {
             mInterstitialAd?.show(this)
         } else {
-            Log.d("TAG", "The interstitial ad wasn't ready yet.")
+            Log.d(TAG, "The interstitial ad wasn't ready yet.")
         }
     }
 
     fun stopInterstitialAdd(){
         if (mInterstitialAd != null) {
-            mInterstitialAd
+            mInterstitialAd = null
         } else {
-            Log.d("TAG", "The interstitial ad wasn't ready yet.")
+            Log.d(TAG, "The interstitial ad wasn't ready yet.")
         }
     }
 
